752f017d62238ae345cd6d1f63da41478f09ea5e
Mohammad Asif <asifansari309753@gmail.com>
2025-12-26 16:47:39 +0530

    Adding aaj wala

--- p62.py
+++ p62.py
@@ -2,7 +2,14 @@ import os
 
 print(os.getcwd())
 
-lst = os.listdir()
-# for file in lst:
-#     print(file)
+# lst = os.listdir()
+# # for file in lst:
+# #     print(file)
+
+# # os.rename("old_file_name","new_file_name")
+# os.rename('p611.py','p61.py')
+
+
+os.remove('p61.py')
+
 
