d100960b7479a4cc664e9888d30bfa514947ff9b
Mohammad Asif <asifansari309753@gmail.com>
2025-12-26 15:14:14 +0530

    Today's files are added

---
+++ p61.py
@@ -0,0 +1,16 @@
+# Python Standard Libraries: OS, sys, math, random, datetime.
+
+def greet():
+    print("Good afternoon")
+
+def goodbye():
+    print("byee")
+
+# greet()
+
+if __name__ == '__main__':
+    greet()
+
+if __name__ == 'p61':
+    goodbye()
+
---
+++ p62.py
@@ -0,0 +1,8 @@
+import os
+
+print(os.getcwd())
+
+lst = os.listdir()
+# for file in lst:
+#     print(file)
+
